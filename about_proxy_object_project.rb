727651fcec5962335234bdf3d1af5cb4d74ef7b0
thisiswei <99plus2@gmail.com>
2012-11-10 21:42:05 -0800

    nov/10/12 9:41pm mountain what? move out the way

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,27 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+  
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def method_missing(name, *args)
+    @messages << name
+    @object.send(name,*args)
+  end
+
+  def called?(method)
+    @messages.include?(method)
+  end
+
+  def number_of_times_called(method)
+    @messages.count(method)
+  end
+
+
 end
 
 # The proxy object should pass the following Koan:
